7bcce5652db61f1228bc3ca91735316d3c5b147d
Juan <juanalexandro.roblesaraiza@gmail.com>
2020-11-01 16:44:46 -0600

    cambio
    cambio

--- src/app/services/chats.service.ts
+++ src/app/services/chats.service.ts
@@ -14,8 +14,8 @@ export class ChatsService {
 
   constructor(private http:HttpClient) { }
 
-  bloquearChat( id_chat:number, id_usuario:number){
-    return this.http.get(`${this.url}chats/bloquearChat.php?id_chat=${id_chat}&id_usuario=${id_usuario}`)//.pipe(retry(3))
+  bloquearChat( id_chat:number){
+    return this.http.get(`${this.url}chats/bloquearChat.php?id_chat=${id_chat}`)//.pipe(retry(3))
   }
 
   cambiarEstadoNotificacion( id_chat:number){
@@ -26,9 +26,8 @@ export class ChatsService {
     return this.http.get(`${this.url}chats/cancelarChat.php?id_chat=${id_chat}&mensaje=${mensaje}`)//.pipe(retry(3))
   }
 
-  confirmarCompra(chat:any){
-    const CHAT = serialize(chat);
-    return this.http.post(`${this.url}chats/confirmarCompra.php`, CHAT)//.pipe(retry(3))
+  confirmarCompra(id_chat:any){
+    return this.http.get(`${this.url}chats/confirmarCompra.php?id_chat=${id_chat}`)//.pipe(retry(3))
   }
 
   enviarMensaje(id_chat:number, mensaje:string){
