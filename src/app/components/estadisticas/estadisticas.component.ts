bfc4188b743556b1701529650abf436e1e5dced9
jessusu20 <c.f.cubers@gmail.com>
2020-11-08 21:29:19 -0600

    estadisticas

--- src/app/components/estadisticas/estadisticas.component.ts
+++ src/app/components/estadisticas/estadisticas.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CasasService } from 'src/app/services/casas.service';
 
 @Component({
   selector: 'app-estadisticas',
@@ -7,9 +9,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EstadisticasComponent implements OnInit {
 
-  constructor() { }
+  tabActive=1;
+  nacionalidades:any=[];
+  casas:any=[];
+  colonias:any=[];
+
+  constructor(private casasService:CasasService, private router:Router ) { }
 
   ngOnInit(): void {
+    this.casasService.getEstadisticasNacionalidades().subscribe(resultado=>{
+      console.log(resultado)
+      this.nacionalidades=resultado
+    })
+    this.casasService.getEstadisticasCasas().subscribe(resultado=>{
+      console.log(resultado)
+      this.casas=resultado
+    })
+    this.casasService.getEstadisticasColonias().subscribe(resultado=>{
+      console.log(resultado)
+      this.colonias=resultado
+    })
+
   }
 
+  mostrarNacionalidades(){
+    this.tabActive=1;
+  }
+  
+  mostrarCasas(){
+    this.tabActive=2;
+  }
+
+  mostrarColonias(){
+    this.tabActive=3;
+  }
+
+  editarCasa(id: number) {
+    this.router.navigate(['editar-casa', id]);
+  }
 }
