3cc7e956540e4bbcf5aac14e3dc95c07ff7c4fbd
jessusu20 <c.f.cubers@gmail.com>
2020-11-08 21:29:55 -0600

    semestres

--- src/app/components/semestres/semestres.component.ts
+++ src/app/components/semestres/semestres.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CasasService } from 'src/app/services/casas.service';
 import { CuartosService } from 'src/app/services/cuartos.service';
 
@@ -13,11 +14,35 @@ export class SemestresComponent implements OnInit {
   @ViewChild('modalSemestre', {static: false}) modalSemestre;
 
   semestres:any=[];
+  enviarForm: FormGroup;
   
-  constructor(private cuartosService:CuartosService, private casasService:CasasService) { }
+  constructor(private cuartosService:CuartosService, private casasService:CasasService,private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.formEnviarInit();
     this.getSemestres();
+    let now=new Date();
+    console.log(now)
+  }
+
+  get validacionNombre() {
+    return this.enviarForm.get('nombre').invalid && this.enviarForm.get('nombre').touched
+  }
+
+  get validacionInicio() {
+    return this.enviarForm.get('inicio').invalid && this.enviarForm.get('inicio').touched
+  }
+  
+  get validacionFin() {
+    return this.enviarForm.get('fin').invalid && this.enviarForm.get('fin').touched
+  }
+
+  formEnviarInit(){
+    this.enviarForm = this.fb.group({
+      nombre: [null, Validators.required],
+      inicio: [null, Validators.required],
+      fin: [null, Validators.required],
+    })
   }
 
   getSemestres(){
@@ -30,8 +55,8 @@ export class SemestresComponent implements OnInit {
     if(window.confirm("¿Esta seguro de querer eliminar este semestre?")){
       this.casasService.eliminarSemestre(id_semestre).subscribe(resultado=>{
         if(resultado==true){
-          window.alert("Semestre eliminado");
           this.getSemestres();
+          window.alert("Semestre eliminado");
         }else{
           window.alert("El semestre no se puede eliminar porque tienes ofertas con este semestre");
         }
@@ -40,7 +65,22 @@ export class SemestresComponent implements OnInit {
   }
 
   agregarSemestre(){
-
+    let now=new Date();
+    let fin=new Date(this.enviarForm.get("fin").value)
+    if(this.enviarForm.get("inicio").value>=this.enviarForm.get("fin").value || fin<now){
+      window.alert("Fechas incorrectas")
+    }else{
+      this.casasService.crearSemestre(this.enviarForm.value).subscribe(resultado=>{
+        console.log(resultado)
+        if(resultado==true){
+          //window.alert("semesre")
+          this.cerrar.nativeElement.click();
+          this.getSemestres();
+        }else{
+          window.alert("Ha ocurrido un error. Intentelo más tarde")
+        }
+      })
+    }
   }
 
 }
